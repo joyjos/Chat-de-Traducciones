13bce1cfbb69160b51ffe3de15b718878468f23c
joyjos <enjoyjos@hotmail.com>
2025-06-14 10:40:09 +0200

    app.js modified

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Importar dependencias
 import express from "express";
 import dotenv from "dotenv";
-import OpenAI from "openai"
+import OpenAI from "openai";
 
 // Cargar configuración API Key
 dotenv.config();
@@ -14,16 +14,29 @@ const PORT = process.env.PORT || 3000;
 app.use("/", express.static("public"));
 
 // Middleware para procesar JSON (convierto JSON a un Objeto de JavaScript)
-app.use(express.json);
+app.use(express.json());
 
 // Instancia de OpenAI y pasarle el API Key
-
+const openai = new OpenAI({
+    apikey: process.env.OPENAI_API_KEY
+});
 
 // Ruta/endpoint/url
-
+app.post("/api/traducir", (req, res) => {
     // Funcionalidad de traducir con IA
+    const {inputText, targetLang} = req.body;
+
+    const promptSystem1 = "Eres un traductor profesional";
+    const promptSystem2 = "Solo puedes responder con una traducción directa del texto que el usuario te envíe"
+                            + "Cualquier otra respuesta o conversación está prohibida";
+    const promptUser =`Traduce el siguiente texto al ${targetLang}: ${inputText}`
     // Llamar al LLM o modelo de OpenAI
 
+    return res.status(200).json({
+        message: "Hola qué tal, soy una ruta en Node",
+        content: req.body
+    });
+})
 
 // Servir el BackEnd
 app.listen(PORT, () =>  {
